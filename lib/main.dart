50718987f10cffcd8540e80cb5155f59f46eceeb
owhiteman <54307171+owhiteman@users.noreply.github.com>
2022-07-09 12:07:06 +1000

    Text functionality for new note

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
     MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const HomePage(),
       routes: {
